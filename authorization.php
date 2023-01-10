b1bd56a3920bd65a3f04a9fd866998c78379886c
Dauren <79454127+DaurenBilal@users.noreply.github.com>
2023-01-10 18:58:20 +0600

    страница регистраций авторизаций добавлены

---
+++ authorization.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="resources/css/auth.css">
+</head>
+<body>
+
+<!-- Форма авторизаций -->
+
+    <form action="" method="">
+        <label for="">Логин</label>
+        <input type="text" placeholder="Введите ваш логин">
+        <label for="">Пароль</label>
+        <input type="password" placeholder="Введите пароль">
+        <button>Войти</button>
+        <p>
+            Увас нету аккаунта? - <a href="http://localhost/subvost/registration.php">Зарегистрироватся</a>
+        </p>
+    </form>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="resources/css/auth.css">
+</head>
+<body>
+
+<!-- Форма авторизаций -->
+
+    <form action="" method="">
+        <label for="">ФИО</label>
+        <input type="text" placeholder="Введите своё полное имя">
+        <label for="">Логин</label>
+        <input type="text" placeholder="Введите ваш логин">
+        <label for="">Почта</label>
+        <input type="email" placeholder="Введите вашу почту">
+        <label for="">Пароль</label>
+        <input type="password" placeholder="Введите пароль">
+        <label for="">Подтверждение пароля</label>
+        <input type="password" placeholder="Подтвердите пароль">
+        <button>Войти</button>
+        <p>
+            Увас нету аккаунта? - <a href="http://localhost/subvost/authorization.php">Зарегистрироватся</a>
+        </p>
+    </form>
+</body>
+</html>
