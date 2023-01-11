14d757f7c0a8b6d94f079c4f1e95733c7b6fda17
Dauren <79454127+DaurenBilal@users.noreply.github.com>
2023-01-11 14:13:58 +0600

    регистрация и авторизация почти готова, осталось решить последнюю проблему

--- authorization.php
+++ authorization.php
@@ -1,23 +1,33 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Авторизация</title>
     <link rel="stylesheet" href="resources/css/auth.css">
 </head>
 <body>
 
 <!-- Форма авторизаций -->
 
-    <form action="" method="">
+    <form action="vendor/signin.php" method="post">
         <label for="">Логин</label>
-        <input type="text" placeholder="Введите ваш логин">
+        <input type="text" name="login" placeholder="Введите ваш логин">
         <label for="">Пароль</label>
-        <input type="password" placeholder="Введите пароль">
-        <button>Войти</button>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <button type="submit">Войти</button>
         <p>
             Увас нету аккаунта? - <a href="http://localhost/subvost/registration.php">Зарегистрироватся</a>
         </p>
+        <?php
+        if (isset($_SESSION['message'])) {
+            echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+        }
+        unset($_SESSION['message']);
+        ?>
     </form>
 </body>
 </html>
--- registration.php
+++ registration.php
@@ -1,29 +1,45 @@
-<!DOCTYPE html>
+<?php
+    session_start();
+    if (isset($_SESSION['user'])) {
+        header('Location: profile.php');
+    }
+?>
+
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Авторизация и регистрация</title>
     <link rel="stylesheet" href="resources/css/auth.css">
 </head>
 <body>
 
-<!-- Форма авторизаций -->
+    <!-- Форма регистрации -->
 
-    <form action="" method="">
-        <label for="">ФИО</label>
-        <input type="text" placeholder="Введите своё полное имя">
-        <label for="">Логин</label>
-        <input type="text" placeholder="Введите ваш логин">
-        <label for="">Почта</label>
-        <input type="email" placeholder="Введите вашу почту">
-        <label for="">Пароль</label>
-        <input type="password" placeholder="Введите пароль">
-        <label for="">Подтверждение пароля</label>
-        <input type="password" placeholder="Подтвердите пароль">
-        <button>Войти</button>
+    <form action="vendor/signup.php" method="post" enctype="multipart/form-data">
+        <label>Никнейм</label>
+        <input type="text" name="nickname" placeholder="Введите свой никнейм">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Почта</label>
+        <input type="email" name="email" placeholder="Введите адрес своей почты">
+        <label>Изображение профиля</label>
+        <input type="file" name="avatar">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <label>Подтверждение пароля</label>
+        <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
+        <button type="submit">Войти</button>
         <p>
-            Увас нету аккаунта? - <a href="http://localhost/subvost/authorization.php">Зарегистрироватся</a>
+            У вас уже есть аккаунт? - <a href="authorization.php">авторизируйтесь</a>!
         </p>
+        <?php
+            if (isset($_SESSION['message'])) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
     </form>
+
 </body>
 </html>
